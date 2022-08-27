33ca4a1eaaa0e62f019fe71869289c6c8e673dc1
Temiloluwa <91268094+Ytemiloluwa@users.noreply.github.com>
2022-08-27 23:53:13 +0100

    Initial Commit

---
+++ GoogleAnimation/GoogleAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleAnimationApp.swift
+//  GoogleAnimation
+//
+//  Created by Temiloluwa on 27/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GoogleAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
